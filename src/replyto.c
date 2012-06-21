be177b4390bc9f7406429d6e65ff032a56e3ea7b
HAYASHI Kentaro <kenhys@gmail.com>
2012-06-22 01:52:45 +0900

    rename g_opt to option

--- src/replyto.c
+++ src/replyto.c
@@ -62,10 +62,7 @@ static SylPluginInfo info = {
   N_(PLUGIN_DESC)
 };
 
-
-
-
-ReplyToOption g_opt;
+ReplyToOption option;
 
 static void compose_created_cb(GObject *obj, gpointer compose);
 static void summaryview_menu_popup_cb(GObject *obj, GtkItemFactory *ifactory,
